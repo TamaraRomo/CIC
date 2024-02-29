87e79b84e6e98acf437e46525f1c17f8ccb8e323
AdrianPalmaSo <94417975+AdrianPalmaSo@users.noreply.github.com>
2024-02-29 12:44:38 -0600

    Datos base datos

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: process.env.DB_HOST, // 'localhost'
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    user: 'admin',
+    password: 'moraazul',
     database: process.env.DB_DATABASE 
 });
 
